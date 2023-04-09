50a5b7720579d5afa118323834fdb78cef1d66d4
OGBASH03 <116046076+OGBASH03@users.noreply.github.com>
2023-04-09 16:47:01 -0400

    finished routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,3 +21,42 @@ api.get('/:id', (req, res) => {
                 : res.json('No note associated with the ID');
         });
 });
+
+//DELETE ROUTES FOR A SPECIFIED ROUTE
+api.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+
+            const result = json.filter((note) => note.id !== noteId);
+
+
+
+            writeToFile('./db/db.json', result);
+
+
+            res.json(result);
+
+        });
+});
+
+// POST ROUTES TO BUILD A NEW NOTE
+api.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    const newNote = {
+
+        id: uuidv4(),
+        title, text
+    };
+
+
+    const parsedData = readAndAppend(newNote, './db/db.json');
+    res.json(parsedData);
+
+});
+
+module.exports = api;
